307e16cc3a96c9aff969514270ed1553c59cece0
dualdier <alexpan13@yahoo.com>
2025-09-03 18:54:20 -0500

    grouped player and draw objects

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
+    
+    
     
     while True:
         for event in pygame.event.get():
@@ -19,9 +25,11 @@ def main():
                 return
         dt = clock.tick(60) / 1000
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-        player.update(dt)
+        
         
     
 
